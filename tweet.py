5360fdce131d5ca750b37feeb6cfdadb12cb11d9
aabhasamol <aabhas4a@gmail.com>
2021-05-01 16:20:38 +0530

    Added Bangalore Functionality as well

--- tweet.py
+++ tweet.py
@@ -3,14 +3,20 @@ import requests
 import datetime
 import time
 from config import *
+from collections import OrderedDict
 
 def get_quote(response):
     data = response.json()
+    dates=''
+    avail=''
     for i in data["centers"]:
+        if i['district_name']=='Bangalore Urban':
+            name='Centre - ' + i['name']+', Bangalore'
+        else:
+            name='Centre - ' + i['name']+', '+ i['district_name']
         
-        name='Vaccination Centre - ' + i['name']+'\nVaccines Available On:'
         price=i["fee_type"]
-        
+ 
         if price=="Paid":
             price = "Vaccination Price = " 
             for f in i['vaccine_fees']: 
@@ -31,11 +37,16 @@ def get_quote(response):
                 price=''
             
             capacity = j['available_capacity']
-            tweetpost=tweetpost+date+' - '+str(capacity)+'\n'
-
-        tweetpost=tweetpost+vax_data+price+'\n'
-        posttweet(tweetpost)
-        time.sleep(60)
+            if capacity!=0:
+                dates+=date+' - '+str(capacity)+'\n'
+                avail='Vaccines Available On:\n'
+        
+        if avail!='':
+            tweetpost=tweetpost+avail+dates+vax_data+price+'\n'
+            dates=''
+            avail=''
+            posttweet(tweetpost)
+            time.sleep(60)
 
 def posttweet(data):
     try:
@@ -48,16 +59,23 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
 
-district_id = 240 # Ranchi
 numdays = 1
 
 base = datetime.datetime.today()
 date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
 date_str = [x.strftime("%d-%m-%Y") for x in date_list]
+district_id=[240, 265] # ranchi, bangalore urban
 
 def action():
-    for datenow in date_str:
-       URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=834001&date={}".format(datenow)
+    pincode=[]
+    for j in district_id:
+        pin = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}'.format(j, date_str[0]))
+        pins = pin.json()
+        for f in pins['centers']:
+            pincode.append(f['pincode'])
+    pincode = list(OrderedDict.fromkeys(pincode))
+    for pin in pincode:
+       URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pin, date_str[0])
        response = requests.get(URL)
        get_quote(response)
     
